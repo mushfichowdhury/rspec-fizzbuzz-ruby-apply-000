f5f512b17735790c7a700a251e74ffe62fe96af5
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-06-12 06:18:36 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,7 +1,7 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 
-def fizzbuzz(input)
+def fizzbuzz(int)
   if input = 3 || fizz_3
     puts "Fizz"
   elsif input = 5 || fizz_5
