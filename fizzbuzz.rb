46dd1f395a8b6fa1c699953653e3f0d7f0ab0dad
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-06-12 06:04:31 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,19 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
+
+def fizzbuzz(input)
+  if input == "3"
+    puts "Fizz"
+  elsif input == "5"
+    puts "Buzz"
+  elsif input == "15"
+    puts "Fizzbuzz"
+  else input == "4"
+    puts "nil"
+  end
+end
+
+fizz_3 = fizzbuzz(3)
+fizz_5 = fizzbuzz(5)
+fizz_15 = fizzbuzz(15)
+fizz_4 = fizzbuzz(4)
